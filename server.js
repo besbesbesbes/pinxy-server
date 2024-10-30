73cd25cf0551db27bdc4407679f3945a4a2f463b
besbesbesbes <smt.bes@gmail.com>
2024-10-30 11:29:06 +0700

    Add file structure

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("---------------Test");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const notFound = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//routing
+app.use(notFound);
+app.use(errorMiddleware);
+
+//start server
+const port = process.env.PORT || 8099;
+app.listen(port, () => console.log("Server on ", port));
